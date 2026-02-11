c883385046f2329c19c7f9892a4d3257e2846c9e
yatniel <yatniel@qq.com>
2026-02-11 01:00:56 -0800

    update

--- main.go
+++ main.go
@@ -6,11 +6,10 @@ import (
 
 	. "github.com/bamgoo/base"
 	_ "github.com/bamgoo/builtin"
-	"github.com/bamgoo/cache"
 	"github.com/bamgoo/mutex"
 
 	"github.com/bamgoo/bamgoo"
-	"github.com/bamgoo/web"
+	"github.com/bamgoo/http"
 )
 
 func main() {
@@ -18,30 +17,37 @@ func main() {
 }
 
 func init() {
-	bamgoo.Register("sys.index", web.Router{
+	bamgoo.Register("index", http.Router{
 		Uri: "/", Name: "首页", Desc: "首页",
-		Action: func(ctx *web.Context) {
-			ctx.Text("hello sys world.")
-		},
-	})
-
-	bamgoo.Register("www.index", web.Router{
-		Uri: "/", Name: "首页", Desc: "首页",
-		Action: func(ctx *web.Context) {
-			cache.Write("key", Map{"msg": "msg from cache."}, time.Second*10)
+		Action: func(ctx *http.Context) {
 			ctx.Text("hello world.")
 		},
 	})
-	bamgoo.Register("www.json", web.Router{
-		Uri: "/json", Name: "JSON", Desc: "JSON",
-		Action: func(ctx *web.Context) {
-			data, _ := cache.Read("key")
-			ctx.Echo(nil, Map{
-				"msg":   "hello world.",
-				"cache": data,
-			})
+
+	bamgoo.Register("www.index", http.Router{
+		Uri: "/test", Name: "首页", Desc: "首页",
+		Action: func(ctx *http.Context) {
+			ctx.Text("hello test world.")
 		},
 	})
+
+	// bamgoo.Register("www.index", http.Router{
+	// 	Uri: "/", Name: "首页", Desc: "首页",
+	// 	Action: func(ctx *http.Context) {
+	// 		cache.Write("key", Map{"msg": "msg from cache."}, time.Second*10)
+	// 		ctx.Text("hello world.")
+	// 	},
+	// })
+	// bamgoo.Register("www.json", http.Router{
+	// 	Uri: "/json", Name: "JSON", Desc: "JSON",
+	// 	Action: func(ctx *http.Context) {
+	// 		data, _ := cache.Read("key")
+	// 		ctx.Echo(nil, Map{
+	// 			"msg":   "hello world.",
+	// 			"cache": data,
+	// 		})
+	// 	},
+	// })
 
 	bamgoo.Register(bamgoo.START, bamgoo.Trigger{
 		Name: "启动", Desc: "启动",
