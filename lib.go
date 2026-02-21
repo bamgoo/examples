5d502689a461876810a0673b0222c91c202384c7
yatniel <yatniel@qq.com>
2026-02-21 12:07:51 -0800

    library

---
+++ lib.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	. "github.com/bamgoo/base"
+	_ "github.com/bamgoo/builtin"
+
+	"github.com/bamgoo/bamgoo"
+)
+
+func init() {
+	bamgoo.Register("sendcloud", bamgoo.Library{
+		Name: "sendcloud", Desc: "SendCloud邮件",
+		Methods: bamgoo.Methods{
+			"send": bamgoo.Method{
+				Name: "发送邮件", Desc: "发送邮件",
+				Action: func(ctx *bamgoo.Context) (Map, Res) {
+					return nil, bamgoo.OK
+				},
+			},
+		},
+	})
+}
